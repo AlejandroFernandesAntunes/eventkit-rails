78819b82c7400e7c6a89dbced0ab0f2d6b3c8b3e
AlejandroFernandesAntunes <aantunes70@hotmail.com>
2022-05-03 16:14:56 -0300

    uglifier

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,6 @@ EventKit::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_files = false
 
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
@@ -77,4 +75,5 @@ EventKit::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.assets.js_compressor = Uglifier.new(harmony: true)
 end
