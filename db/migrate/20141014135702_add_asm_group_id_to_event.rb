5893cc7e06dbc14c85cad1466ee2a12dc92316f9
AlejandroFernandesAntunes <aantunes70@hotmail.com>
2022-05-03 16:29:04 -0300

    target migrations

--- db/migrate/20141014135702_add_asm_group_id_to_event.rb
+++ db/migrate/20141014135702_add_asm_group_id_to_event.rb
@@ -1,4 +1,4 @@
-class AddAsmGroupIdToEvent < ActiveRecord::Migration
+class AddAsmGroupIdToEvent < ActiveRecord::Migration[7.0]
   def change
     add_column :events, :asm_group_id, :integer, :limit => 2
   end
